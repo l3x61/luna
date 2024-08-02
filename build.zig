4122ae70b5a01459b1068f10a25d7330064ed4fc
l3x61 <me@l3x61.dev>
2024-08-02 12:57:40 +0200

    rename project, bump version

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "moth",
+        .name = "luna",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
