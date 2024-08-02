07c37e127433a6ff9688c0f642f4bf1cca80292c
l3x61 <me@l3x61.dev>
2024-08-02 04:20:37 +0200

    read print loop

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello World", .{});
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    repl: while (true) {
+        try stdout.print("> ", .{});
+        var buffer: [1024]u8 = undefined;
+        const line = try stdin.readUntilDelimiter(&buffer, '\n');
+        if (line.len == 0) {
+            continue :repl;
+        }
+        if (std.mem.eql(u8, line, "exit")) {
+            break :repl;
+        }
+
+        try stdout.print("{s}\n", .{line});
+    }
 }
