4e4862a14ea8a903b7566ddf9b0a29f96c0248b4
l3x61 <me@l3x61.dev>
2024-09-01 00:04:57 +0200

    refactor token

--- src/token.zig
+++ src/token.zig
@@ -72,20 +72,20 @@ pub const Token = struct {
 
     pub fn showInSource(self: Token, color: []const u8) void {
         var cursor: usize = 0;
-        var lineStart: usize = 0;
+        var line_start: usize = 0;
         var line: usize = 1;
         const start_index = @intFromPtr(self.lexeme.ptr) - @intFromPtr(self.source.ptr);
         while (cursor < start_index) {
             if (self.source[cursor] == '\n') {
                 line += 1;
-                lineStart = cursor + 1;
+                line_start = cursor + 1;
             }
             cursor += 1;
         }
-        var lineEnd = start_index;
-        while (lineEnd < self.source.len and self.source[lineEnd] != '\n') lineEnd += 1;
-        const before_lexeme = self.source[lineStart..start_index];
-        const after_lexeme = self.source[start_index + self.lexeme.len .. lineEnd];
+        var line_end = start_index;
+        while (line_end < self.source.len and self.source[line_end] != '\n') line_end += 1;
+        const before_lexeme = self.source[line_start..start_index];
+        const after_lexeme = self.source[start_index + self.lexeme.len .. line_end];
         std.debug.print(Ansi.Yellow ++ "{d: >4}" ++ Ansi.Reset ++ " | {s}{s}{s}" ++ Ansi.Reset ++ "{s}\n", .{ line, before_lexeme, color, self.lexeme, after_lexeme });
         std.debug.print(Ansi.Yellow ++ "{[e]s: >4}" ++ Ansi.Reset ++ " | {[e]s: >[before]}{[color]s}{[e]s:~>[token]}" ++ Ansi.Reset ++ "{[e]s: >[after]}\n", .{ .e = "", .before = before_lexeme.len, .token = if (self.lexeme.len == 0) 1 else self.lexeme.len, .after = after_lexeme.len, .color = color });
     }
